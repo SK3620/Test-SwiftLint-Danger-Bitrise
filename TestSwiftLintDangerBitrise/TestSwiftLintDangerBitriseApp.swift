bbddefbe710478bd82cb5150fa23762e9ded9118
SK3620 <k.n.t11193620@gmail.com>
2025-05-14 11:22:25 +0900

    initial commit

---
+++ TestSwiftLintDangerBitrise/TestSwiftLintDangerBitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftLintDangerBitriseApp.swift
+//  TestSwiftLintDangerBitrise
+//
+//  Created by 鈴木 健太 on 2025/05/14.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftLintDangerBitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestSwiftLintDangerBitriseTests/TestSwiftLintDangerBitriseTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestSwiftLintDangerBitriseTests.swift
+//  TestSwiftLintDangerBitriseTests
+//
+//  Created by 鈴木 健太 on 2025/05/14.
+//
+
+import Testing
+@testable import TestSwiftLintDangerBitrise
+
+struct TestSwiftLintDangerBitriseTests {
+
+    @Test func example() async throws {
+        #expect(true)
+    }
+}
